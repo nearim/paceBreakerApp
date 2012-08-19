6a30064eafe8d1518ffc68051aeca73db9ebb1d3
Nicholas Shelly <nshelly@apple.com>
2012-08-18 17:20:25 -0700

    Added view-based app with tab-controlled view.

---
+++ PacebreakerTests.m
@@ -0,0 +1,32 @@
+//
+//  PacebreakerTests.m
+//  PacebreakerTests
+//
+//  Created by Nicholas on 8/18/12.
+//  Copyright (c) 2012 Nicholas. All rights reserved.
+//
+
+#import "PacebreakerTests.h"
+
+@implementation PacebreakerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PacebreakerTests");
+}
+
+@end
